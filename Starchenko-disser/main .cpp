92bd6f139664375b72fd678892001d5657dbe6aa
Андрей Чусов <lpsztemp@hotmail.com>
2020-09-15 16:03:52 +1000

    get_result_and_time

--- Starchenko-disser/main .cpp
+++ Starchenko-disser/main .cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdint>
 #include <iomanip>
+#include <chrono>
 using namespace std;
 
 /*
@@ -48,6 +49,23 @@ uint64_t crc_1(uint64_t input, uint64_t poly)
 	return -1;
 }
 
+struct result_and_time
+{
+	int result;
+	std::chrono::milliseconds time;
+};
+
+result_and_time get_result_and_time(const uint8_t* V, unsigned N, int (*compute) (const uint8_t* , unsigned))
+{
+	result_and_time value;
+	auto tm0 = std::chrono::system_clock::now();
+
+	value.result = compute(V, N);
+
+	value.time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - tm0);
+
+	return value;
+}
 
 int main(int argc, char** argv)
 {
@@ -56,5 +74,12 @@ int main(int argc, char** argv)
 	cout << "Parity bit of " << hex << input << " is " << parity_bit(input) << "\n";
 	cout << "Parity bit of " << hex << input << " is " << parity_bit_vector((uint8_t*) &input, sizeof(input)) << "\n";
 	cout << "CRC of " << hex << input << " with polynomial " << hex << poly << " is " << crc_1(input, poly) << "\n";
+
+	result_and_time res;
+	res = get_result_and_time((uint8_t*) &input, sizeof(input), parity_bit_vector);
+	cout << "Result: " << res.result << ", time: " << res.time.count() << "ms.\n";
+	res = get_result_and_time((uint8_t*) &input, sizeof(input), accum_bit_vector);
+	cout << "Result: " << res.result << ", time: " << res.time.count() << "ms.\n";
+
 	return 0;
 }
